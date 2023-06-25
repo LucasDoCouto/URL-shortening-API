fe60e7331ccc2b28e795e918c89136297130fc84
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-06-25 17:15:08 -0400

    Add: mobile menu

---
+++ index.js
@@ -0,0 +1,5 @@
+function menuOnClick() {
+    // alert("teste")
+    document.getElementById("hamburguer-btn").classList.toggle("clicked")
+    document.getElementById("dropdown-mobile").classList.toggle("dropdown-closed")
+  }
